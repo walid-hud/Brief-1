389ae8763dd98c4d6f9427fceb7a367c80884e5b
walid O <164819061+walid-hud@users.noreply.github.com>
2026-01-21 12:53:44 +0100

    typo fix

--- src/utils/TailwindElement.ts
+++ src/utils/TailwindElement.ts
@@ -1,6 +1,6 @@
 import { LitElement,  } from "lit";
 // import tailwindStyles from "../index.css?inline";
-// Import tailwind like this does work but any font imports are disabled, so we just opt out of shadow dom and let the global 
+// Importing tailwind like this does work but any font imports are disabled, so we just opt out of shadow dom and let the global 
 // styles take effect for now
 
 class TailwindElement extends LitElement {
