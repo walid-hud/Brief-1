56ace8a0ff770b4e190432a73f22c111b474a921
walid O <164819061+walid-hud@users.noreply.github.com>
2026-01-21 12:53:16 +0100

    some clarity on the tailwind component implementation
    .

--- src/utils/TailwindElement.ts
+++ src/utils/TailwindElement.ts
@@ -1,5 +1,7 @@
 import { LitElement,  } from "lit";
 // import tailwindStyles from "../index.css?inline";
+// Import tailwind like this does work but any font imports are disabled, so we just opt out of shadow dom and let the global 
+// styles take effect for now
 
 class TailwindElement extends LitElement {
   // static styles = css`
